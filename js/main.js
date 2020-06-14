a23be799bae132c549d37f757048d42918ad9acf
Michaelmelloy <michael.deiwald.02@gmail.com>
2020-06-14 15:04:22 +0200

    Fixes and scrollbar

--- js/main.js
+++ js/main.js
@@ -11,10 +11,12 @@ function hideBar()
     {
         // Hide Navbar
         document.getElementsByClassName("header")[0].style.opacity = 0;
+        document.getElementsByClassName("scroll")[0].style.transform = "rotate(180deg)"
     }else
     {
         // Show Navbar
         document.getElementsByClassName("header")[0].style.opacity = 1;
+        document.getElementsByClassName("scroll")[0].style.transform = "rotate(0deg)"
     }
 }
 
@@ -23,4 +25,23 @@ function thisEventListener()
     window.addEventListener('scroll', hideBar)
     {
     }
+}
+
+window.onscroll = function()
+{
+    var yPosition = window.scrollY;
+    var maxScrollHeight = $(document).height() - $(window).height();
+
+    percentage = (yPosition * 100) / maxScrollHeight;
+
+    if(percentage > 96)
+    {
+        percentage = 96;
+    }
+    if(percentage < 1)
+    {
+        percentage = 1;
+    }
+
+    document.getElementsByClassName("scroll")[0].style.top = percentage+"vh";
 }
